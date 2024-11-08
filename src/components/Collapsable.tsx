3343929329f36b6c8ab02319a063b2197c5cf12b
jnkoriginals <jan.thieme@icloud.com>
2024-11-08 17:04:31 +0100

    update - small improvements

--- src/components/Collapsable.tsx
+++ src/components/Collapsable.tsx
@@ -46,7 +46,35 @@ export const Collapsable: React.FC<IProps> = ({ open, title, children }) => {
                         onClick={() => handleFilterOpening()}
                     >
                         <h6 className=''>{title}</h6>
-                        <p>{!isOpen ? "open" : "close"}</p>
+                        <p className='flex items-center justify-center'>
+                            {!isOpen ? (
+                                <span className='w-3 h-3 flex text-current'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        viewBox='0 0 500 320.7'
+                                        fill='currentColor'
+                                    >
+                                        <polygon
+                                            points='70.7 320.7 250 141.4 429.3 320.7 500 250 250 0 0 250 70.7 320.7'
+                                            strokeWidth='0'
+                                        />
+                                    </svg>
+                                </span>
+                            ) : (
+                                <span className='w-3 h-3 flex'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        viewBox='0 0 500 320.7'
+                                        fill='currentColor'
+                                    >
+                                        <polygon
+                                            points='429.3 0 250 179.3 70.7 0 0 70.7 250 320.7 500 70.7 429.3 0'
+                                            strokeWidth='0'
+                                        />
+                                    </svg>
+                                </span>
+                            )}
+                        </p>
                     </div>
                 </div>
                 <div
