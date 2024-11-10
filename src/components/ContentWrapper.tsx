7a329cf5c8d07282ac020562c8dd0bc517c4796c
jnkoriginals <jan.thieme@icloud.com>
2024-11-10 17:27:51 +0100

    fix - responsiveness

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -6,7 +6,7 @@ interface ContentWrapperProps {
 
 export const ContentWrapper: React.FC<ContentWrapperProps> = ({ children }) => {
     return (
-        <div className='m-auto w-full mx-2 h-min mt-5 mb-16 md:mt-20 md:w-3/4 rounded-lg p-3 dark:bg-neutral-900 bg-neutral-50 shadow-md'>
+        <div className='md:m-auto w-full m-2 h-min md:mb-16 md:mt-20 md:w-3/4 rounded-lg p-3 dark:bg-neutral-900 bg-neutral-50 shadow-md'>
             {children}
         </div>
     );
